cf1973bae6c48b28e3ad34d03b2621cf94ce30ab
Yashwanth <Yashwanth@10.0.0.29>
2022-09-19 07:26:05 +0100

    Boot project initial commit

---
+++ demo/src/test/java/com/yash/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
