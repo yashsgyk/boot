ab114c1c5453da06f6a9894d270c0116fac5c621
Yashwanth <Yashwanth@10.0.0.29>
2022-09-27 10:03:21 +0100

    testing merge

--- demo/src/main/java/com/yash/demo/controller/EmployeeController.java
+++ demo/src/main/java/com/yash/demo/controller/EmployeeController.java
@@ -12,7 +12,7 @@ public class EmployeeController {
 	@GetMapping("/employee")
 	public String getEmployees() {
 		System.out.println("Getting list of employees");
-		return "list of employees";
+		return "list employees from db";
 	}
 	
 	@GetMapping("/employee/{id}")
